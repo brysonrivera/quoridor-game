795aec8fe1beb34c45f39c67612d1ad48d1b430e
Bryson Rivera <brysonm.rivera@gmail.com>
2022-06-16 22:13:50 -0700

    Unit Tests Added and ReadMe updated
    Added in some Unit Tests to help with refactoring and updated readme, as there was an error with one of the methods listed in regards to the number of parameters required.

--- README.md
+++ README.md
@@ -35,21 +35,12 @@ When each player tries to place a fence on the board, the position of the fence 
 ## Implementation
 To play we use the following methods:
 
-Method Name   | Parameters
-------------- | -------------
-`move_pawn`   | method takes following two parameters in order: an integer that represents which player (1 or 2) is making the move and a tuple with the coordinates of where the pawn is going to be moved to.     
-`place_fence` |  method takes following two parameters in order: an integer that represents which player (1 or 2) is making the move and a tuple with the coordinates of where the pawn is going to be moved to.
-`is_winner`   | method that takes a single integer representing the player number (1 or 2) as a parameter and returns `True` if that player has won and `False` if that player has not won.
-`visualize_board` | method takes no parameters. Method prints a physical representation of the board with the current location of each pawn on the board.
-
-Here are the return values for each method:
-
-Method Name | Return Values
------------ | -----------
-`move_pawn` | If the move is forbidden by the rule or blocked by the fence, return `False`. If the move was successful or if the move makes the player win, return `True`. If the game has been already won, return `False`.
-`place_fence`| If player has no fence left, or if the fence is out of the boundaries of the board, or if there is already a fence there and the new fence will overlap or intersect with the existing fence, return `False`. If the fence can be placed, return `True`. If the game has been already won, return `False`
-`is_winner` | returns `True` if that player has won and `False` if that player has not won.
-`visualize_board` | returns `None`
+Method Name   | Parameters     |   Return Values  |
+------------- | -------------  | ---------------- |
+`move_pawn`   | method takes following two parameters in order: an integer that represents which player (1 or 2) is making the move and a tuple with the coordinates of where the pawn is going to be moved to. | If the move is forbidden by the rule or blocked by the fence, return `False`. If the move was successful or if the move makes the player win, return `True`. If the game has been already won, return `False`.
+`place_fence` |  method takes following 3 parameters in order: an integer that represents which player (1 or 2) is making the move, a string 'v' (vertical) or 'h' (horizontal), and a tuple with the coordinates of where the pawn is going to be moved to. | If player has no fence left, or if the fence is out of the boundaries of the board, or if there is already a fence there and the new fence will overlap or intersect with the existing fence, return `False`. If the fence can be placed, return `True`. If the game has been already won, return `False`
+`is_winner`   | method that takes a single integer representing the player number (1 or 2) as a parameter and returns `True` if that player has won and `False` if that player has not won. | returns `True` if that player has won and `False` if that player has not won.
+`visualize_board` | method takes no parameters. Method prints a physical representation of the board with the current location of each pawn on the board. | returns `None`
 
 ## How the Game is played
 
---
+++ test_quoridor_game.py
@@ -0,0 +1,294 @@
+from Quoridor import QuoridorGame
+
+
+class TestClass:
+    """
+    Tests include
+    - Turn Validation
+    - Movement Placement Validation (pawns can move orthogonally one space at a time)
+    - Fence Placemenet Validation (players can place their pawns either horizontally or vertically)
+    - Fence Capacity Validation (app keeps track of the number of fences a player has placed/ has available.)
+    """
+
+    def test_one(self):
+        """If player 1 moves to a valid space on their turn, return True"""
+        q = QuoridorGame()
+        bool = q.move_pawn(1, (4, 1))
+        assert bool is True
+
+    def test_two(self):
+        """If player 2 moves to a valid space on their turn, return True """
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        bool = q.move_pawn(2, (5, 8))
+        assert bool is True
+
+    def test_three(self):
+        """If player 1 makes invalid move, return False"""
+        q = QuoridorGame()
+        bool = q.move_pawn(1, (2, 1))
+        assert bool is False
+
+    def test_four(self):
+        """If player 2 makes invalid move, return False"""
+        q = QuoridorGame()
+        q.move_pawn(1, (4, 1))
+        bool = q.move_pawn(2, (3, 7))
+        assert bool is False
+
+    def test_five(self):
+        """If player 1 tries to move out of turn, return false"""
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        bool = q.move_pawn(1, (3, 1))
+        assert bool is False
+
+    def test_six(self):
+        """If player 1 tries to place a fence out of turn, return false"""
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        bool = q.place_fence(1, 'h', (3, 3))
+        assert bool is False
+
+    def test_seven(self):
+        """If player 2 tries to move out of turn, return false"""
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        q.move_pawn(2, (3, 8))
+        bool = q.move_pawn(2, (2, 8))
+        assert bool is False
+
+    def test_eight(self):
+        """If player 1 tries to move to the same space they are currently in, return False"""
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        q.move_pawn(2, (4, 7))
+        bool = q.move_pawn(1, (3, 0))
+        assert bool is False
+
+    def test_nine(self):
+        """If player 2 tries to move to the same space they are currently in, return False"""
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        q.move_pawn(2, (4, 7))
+        q.move_pawn(1, (2, 0))
+        bool = q.move_pawn(2, (4, 7))
+        assert bool is False
+
+    def test_ten(self):
+        """If player 1 tries to place a fence in a space where they previously placed a fence, return False"""
+        q = QuoridorGame()
+        q.place_fence(1, 'h', (3, 0))
+        q.move_pawn(2, (4, 7))
+        bool = q.place_fence(1, 'h', (3, 0))
+        assert bool is False
+
+    def test_eleven(self):
+        """If player 2 tries to place a fence in a space where they previously placed a fence, return False """
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        q.place_fence(2, 'v', (4, 7))
+        q.move_pawn(1, (3, 1))
+        bool = q.place_fence(2, 'v', (4, 7))
+        assert bool is False
+
+    def test_twelve(self):
+        """If a player tries to place a fence in a space where a fence was previously placed by the opposite player, return False"""
+        q = QuoridorGame()
+        q.place_fence(1, 'v', (4, 7))
+        bool = q.place_fence(2, 'v', (4, 7))
+        assert bool is False
+
+    def test_thirteen(self):
+        """A player can place at most 10 fences on the board. Here we are testing whether a player will receive any errors
+        when they try to place all the fences they have available. If test returns no errors, player is 
+        able to successfully place all their fences in unique positions in the board"""
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        q.place_fence(2, 'v', (1, 1))
+        q.move_pawn(1, (2, 0))
+        q.place_fence(2, 'h', (2, 1))
+        q.move_pawn(1, (1, 0))
+        q.place_fence(2, 'v', (3, 1))
+        q.move_pawn(1, (0, 0))
+        q.place_fence(2, 'h', (4, 1))
+        q.move_pawn(1, (0, 1))
+        q.place_fence(2, 'v', (5, 1))
+        q.move_pawn(1, (0, 2))
+        q.place_fence(2, 'h', (6, 1))
+        q.move_pawn(1, (0, 3))
+        q.place_fence(2, 'v', (7, 1))
+        q.move_pawn(1, (0, 4))
+        q.place_fence(2, 'h', (8, 1))
+        q.move_pawn(1, (0, 5))
+        q.place_fence(2, 'v', (1, 2))
+        q.move_pawn(1, (0, 6))
+        bool = q.place_fence(2, 'h', (2, 2))
+        assert bool is True
+
+    def test_fourteen(self):
+        """If player tries to place more fences than their max (10) available, it should return false"""
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        q.place_fence(2, 'v', (1, 1))
+        q.move_pawn(1, (2, 0))
+        q.place_fence(2, 'h', (2, 1))
+        q.move_pawn(1, (1, 0))
+        q.place_fence(2, 'v', (3, 1))
+        q.move_pawn(1, (0, 0))
+        q.place_fence(2, 'h', (4, 1))
+        q.move_pawn(1, (0, 1))
+        q.place_fence(2, 'v', (5, 1))
+        q.move_pawn(1, (0, 2))
+        q.place_fence(2, 'h', (6, 1))
+        q.move_pawn(1, (0, 3))
+        q.place_fence(2, 'v', (7, 1))
+        q.move_pawn(1, (0, 4))
+        q.place_fence(2, 'h', (8, 1))
+        q.move_pawn(1, (0, 5))
+        q.place_fence(2, 'v', (1, 2))
+        q.move_pawn(1, (0, 6))
+        q.place_fence(2, 'h', (2, 2))
+        q.move_pawn(1, (0, 7))
+        # 11th fence for player 2. invalid move
+        bool = q.place_fence(2, 'v', (3, 2))
+        assert bool is False
+
+    def test_fifteen(self):
+        """If a player tries to share the same space with the oppsite player, return False"""
+        q = QuoridorGame()
+        q.move_pawn(1, (4, 1))
+        q. move_pawn(2, (4, 7))
+        q.move_pawn(1, (4, 2))
+        q.move_pawn(2, (4, 6))
+        q.move_pawn(1, (4, 3))
+        q.move_pawn(2, (4, 5))
+        q.move_pawn(1, (4, 4))
+        bool = q.move_pawn(2, (4, 4))
+        assert bool is False
+
+    def test_sixteen(self):
+        """EDGE CASE -- When a player is orthogonally adjacent to another player (right next to each other), if a player is able
+        to jump over another pawn, return True -- """
+        q = QuoridorGame()
+        q.move_pawn(1, (4, 1))
+        q. move_pawn(2, (4, 7))
+        q.move_pawn(1, (4, 2))
+        q.move_pawn(2, (4, 6))
+        q.move_pawn(1, (4, 3))
+        q.move_pawn(2, (4, 5))
+        q.move_pawn(1, (4, 4))
+        bool = q.move_pawn(2, (4, 3))
+        assert bool is True
+
+    def test_seventeen(self):
+        """EDGE CASE: In a specific instance where a player is orthogonally adjacent to their opponent (right next to a player), but there is a 
+        fence behind the opponent, a player is allowed to move diagonally (if player is in front of opponent, they are now next to each other horizontally). 
+        When the above rule is implemented, this test case should return True. """
+        q = QuoridorGame()
+        q.move_pawn(1, (4, 1))
+        q. move_pawn(2, (4, 7))
+        q.move_pawn(1, (4, 2))
+        q.move_pawn(2, (4, 6))
+        q.move_pawn(1, (4, 3))
+        q.move_pawn(2, (4, 5))
+        q.move_pawn(1, (4, 4))
+        q.place_fence(2, "h", (4, 6))
+        bool = q.move_pawn(1, (5, 5))
+        assert bool is True
+
+    def test_eighteen(self):
+        """If a player tries moving to a space where a fence is blocking them to move to that space, return False"""
+        q = QuoridorGame()
+        q.place_fence(1, "h", (4, 8))
+        bool = q.move_pawn(2, (4, 7))
+        assert bool is False
+
+    def test_nineteen(self):
+        """if a player has not won, the method .is_winner(player) should return False. Only way for a player to win is if they have reached their opponents baseline."""
+        q = QuoridorGame()
+        q.move_pawn(1, (4, 1))
+        q. move_pawn(2, (4, 7))
+        q.move_pawn(1, (4, 2))
+        q.move_pawn(2, (4, 6))
+        q.move_pawn(1, (4, 3))
+        q.move_pawn(2, (4, 5))
+        q.move_pawn(1, (4, 4))
+        q.move_pawn(2, (4, 3))
+        bool = q.is_winner(1)
+        assert bool is False
+
+    def test_twenty(self):
+        """If a player has won, then .is_winner(player) should return True"""
+        q = QuoridorGame()
+        q.move_pawn(1, (4, 1))
+        q. move_pawn(2, (4, 7))
+        q.move_pawn(1, (4, 2))
+        q.move_pawn(2, (4, 6))
+        q.move_pawn(1, (4, 3))
+        q.move_pawn(2, (4, 5))
+        q.move_pawn(1, (4, 4))
+        q.move_pawn(2, (4, 3))
+        q.move_pawn(1, (4, 5))
+        q.move_pawn(2, (4, 2))
+        q.move_pawn(1, (4, 6))
+        q.move_pawn(2, (4, 1))
+        q.move_pawn(1, (4, 7))
+        q.move_pawn(2, (4, 0))
+        bool = q.is_winner(2)
+        assert bool is True
+
+    def test_twenty_one(self):
+        """If a player has won, then .is_winner(player) should return False if the parameter value for the method is not the correct player. """
+        q = QuoridorGame()
+        q.move_pawn(1, (4, 1))
+        q. move_pawn(2, (4, 7))
+        q.move_pawn(1, (4, 2))
+        q.move_pawn(2, (4, 6))
+        q.move_pawn(1, (4, 3))
+        q.move_pawn(2, (4, 5))
+        q.move_pawn(1, (4, 4))
+        q.move_pawn(2, (4, 3))
+        q.move_pawn(1, (4, 5))
+        q.move_pawn(2, (4, 2))
+        q.move_pawn(1, (4, 6))
+        q.move_pawn(2, (4, 1))
+        q.move_pawn(1, (4, 7))
+        q.move_pawn(2, (4, 0))
+        bool = q.is_winner(1)
+        assert bool is False
+
+    def test_twenty_two(self):
+        """If a player has reached the opponents baseline, that player has won. When a player reaches another players base coordinate, any other move after
+        should not matter, and we should return False"""
+        q = QuoridorGame()
+        q.move_pawn(1, (4, 1))
+        q. move_pawn(2, (4, 7))
+        q.move_pawn(1, (4, 2))
+        q.move_pawn(2, (4, 6))
+        q.move_pawn(1, (4, 3))
+        q.move_pawn(2, (4, 5))
+        q.move_pawn(1, (4, 4))
+        q.move_pawn(2, (4, 3))
+        q.move_pawn(1, (4, 5))
+        q.move_pawn(2, (4, 2))
+        q.move_pawn(1, (4, 6))
+        q.move_pawn(2, (4, 1))
+        q.move_pawn(1, (4, 7))
+        q.move_pawn(2, (4, 0))
+        bool = q.move_pawn(1, (4, 8))
+        assert bool is False
+
+    def test_twenty_three(self):
+        """If a player attempts to move out of range, they will run into s"""
+        q = QuoridorGame()
+        q.move_pawn(1, (3, 0))
+        q. move_pawn(2, (4, 7))
+        q.move_pawn(1, (2, 0))
+        q.move_pawn(2, (4, 6))
+        q.move_pawn(1, (1, 0))
+        q.move_pawn(2, (4, 5))
+        q.move_pawn(1, (0, 0))
+        q.move_pawn(2, (4, 3))
+        bool = q.move_pawn(1, (8, 0))
+        assert bool is False
